57289c316b4d0e04ace30f1955e1ba27c7d04e11
baekseungho <vudrkd1580@naver.com>
2023-03-14 02:51:24 +0900

    load object

--- movieapp/src/App.js
+++ movieapp/src/App.js
@@ -1,8 +1,45 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [loading, setLoading] = useState(ture);
-  return <div className="App"></div>;
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+      )
+    ).json();
+
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  console.log(movies);
+  return (
+    <div className="App">
+      {loading ? (
+        <h1>loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
